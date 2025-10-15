fc00ba786a4cdce2b392eba6a51d35f3f8c6deaf
ajay9177 <ajay098251@gmail.com>
2025-10-15 12:13:16 +0530

    Completed First 2 Sections

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3) //code readability should be high
+
+console.log("hitesh")
+
+let name = "hitesh" //String data type
+let age = 20 //Number
+let isLoggedIn = false //Boolean
+let state;
+
+//number => 2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof "hitesh") //string
+console.log(typeof age) //number
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
